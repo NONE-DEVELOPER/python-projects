95c5c26bba117851fa5b5df86481aa7557ebb34b
NONE-DEVELOPER <135344570+NONE-DEVELOPER@users.noreply.github.com>
2023-06-24 15:34:16 +0530

    Update Email_Automation.py

--- Email_Automation.py
+++ Email_Automation.py
@@ -9,6 +9,7 @@ def sendEmail(to,message):
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
     server.login('sender-Email','password')
+    #Here enter the gmail and password of the sender with in the single quotes 
     server.sendmail('sender-Email',to,message)
     server.close()
 
